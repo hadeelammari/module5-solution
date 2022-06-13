76da02ce8cd9538bfc2118cef3486a039b1406b2
Hadeel Ammari <hadeel.ammari@gmail.com>
2022-06-13 16:09:11 -0700

    changes to script.js

--- js/script.js
+++ js/script.js
@@ -81,7 +81,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
 // *** start ***
 // On first load, show home view
 showLoading("#main-content");
-console.log("Starting step 1");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
   buildAndShowHomeHTML, // ***** <---- TODO: STEP 1: Substitute [...] ******
